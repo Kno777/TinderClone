6e8119e98531878037e7cdcf86497b9c91e031ba
kno777 <kno0109@mail.ru>
2023-11-13 17:18:55 +0400

    Multiple Cards and User Model

--- TinderClone/HomeViewController.swift
+++ TinderClone/HomeViewController.swift
@@ -7,11 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
     
     let topStackView = TopNavigationStackView()
     let cardsDeckView = UIView()
     let buttonsStackView = HomeBottomControlsStackView()
+    
+    let users = [
+        User(name: "Kelly", age: 23, profession: "Music DJ", imageName: "lady5c"),
+        User(name: "Jane", age: 18, profession: "Teacher", imageName: "lady4c"),
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,11 +43,24 @@ class ViewController: UIViewController {
     }
     
     fileprivate func setupDummyCards() {
-        print("Settings up dummy cards")
         
-        let cardView = CardView(frame: .zero)
-        cardsDeckView.addSubview(cardView)
-        cardView.fillSuperview()
+        users.forEach { user in
+            let cardView = CardView(frame: .zero)
+            cardView.imageView.image = UIImage(named: user.imageName)
+            //cardView.informationLabel.text = "\(user.name) \(user.age)\n\(user.profession)"
+            
+            let attributedText = NSMutableAttributedString(string: user.name, attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 32)])
+            
+            attributedText.append(NSAttributedString(string: " \(user.age)", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 24, weight: .regular)]))
+            
+            attributedText.append(NSAttributedString(string: "\n\(user.profession)", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16)]))
+            
+            cardView.informationLabel.attributedText = attributedText
+            
+            cardsDeckView.addSubview(cardView)
+            cardView.fillSuperview()
+        }
+        
     }
 }
 
---
+++ TinderClone/Model/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  TinderClone
+//
+//  Created by Kno Harutyunyan on 13.11.23.
+//
+
+import Foundation
+
+struct User {
+    
+    // defining our properties for our model layer
+    let name: String
+    let age: Int
+    let profession: String
+    let imageName: String
+}
+
+
