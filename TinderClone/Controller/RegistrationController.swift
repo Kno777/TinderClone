9066f685143758ef4c929b95394b67197aef1832
kno777 <kno0109@mail.ru>
2023-11-14 16:06:42 +0400

    Keyboard Management Notification Observers

--- TinderClone/Controller/RegistrationController.swift
+++ TinderClone/Controller/RegistrationController.swift
@@ -44,21 +44,85 @@ class RegistrationController: UIViewController {
         return tf
     }()
     
+    let registerButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Register", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .heavy)
+        button.backgroundColor = UIColor(red: 253/255, green: 91/255, blue: 95/255, alpha: 1)
+        button.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        button.layer.cornerRadius = 22
+        return button
+    }()
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupGradientLayer()
-        view.backgroundColor = .red
+
+        setupLayout()
         
-        let stackView = UIStackView(arrangedSubviews: [
-            selectPhotoButton,
-            fullNameTextField,
-            emailTextField,
-            passwordTextField
-        ])
+        setupNotificationObserver()
         
+        setupTapGesture()
+    }
+    
+    // MARK: - Functions
+    
+    fileprivate func setupTapGesture() {
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleKeyboardDismiss)))
+    }
+    
+    @objc fileprivate func handleKeyboardDismiss() {
+        self.view.endEditing(true) // dismisses keyboard
+    }
+    
+    fileprivate func setupNotificationObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc fileprivate func handleKeyboardHide() {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut) {
+            self.view.transform = .identity // view come back
+        }
+    }
+    
+    @objc fileprivate func handleKeyboardShow(notification: Notification) {
+        // how to figure out how tall the keyboard actualli is?
+        
+        guard let value = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardFrame = value.cgRectValue
+        print(keyboardFrame)
+        
+        // let's try to figure out how tall the gap is from the register button to the bottom of the screen
+        
+        let bottomSpace = view.frame.height - stackView.frame.origin.y - stackView.frame.height
+        
+        print(bottomSpace)
+        
+        let difference = keyboardFrame.height - bottomSpace
+        
+        self.view.transform = CGAffineTransform(translationX: 0, y: -difference - 8)
+    }
+    
+    lazy var stackView = UIStackView(arrangedSubviews: [
+        selectPhotoButton,
+        fullNameTextField,
+        emailTextField,
+        passwordTextField,
+        registerButton
+    ])
+    
+    fileprivate func setupLayout() {
         stackView.axis = .vertical
         stackView.spacing = 8
         view.addSubview(stackView)
